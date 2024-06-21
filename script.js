b37c8514ad4c4284229928223d83b4aae4f1eb69
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-06-21 15:34:30 +0200

    Update script.js
    Creates empty array for allSongs

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const playlistSongs = document.getElementById('playlist-songs');
 const playButton = document.getElementById('play');
 const pauseButton = document.getElementById('pause');
-const nextButton = document.getElementById("next");
-const previousButton = document.getElementById("previous");
-const shuffleButton = document.getElementById("shuffle");
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+const shuffleButton = document.getElementById('shuffle');
+
+let allSongs = [];
