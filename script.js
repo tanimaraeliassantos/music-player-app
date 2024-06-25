940e4be71f5ce184c302bdab468a63cc760fa054
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-06-25 22:07:46 +0200

    Update script.js
    Adds third object to array

--- script.js
+++ script.js
@@ -20,4 +20,11 @@ let allSongs = [
 		duration: '4:15',
 		src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
 	},
+	{
+		id: 2,
+		title: 'Still Learning',
+		artist: 'Quincy Larson',
+		duration: '3:51',
+		src: 'https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3',
+	},
 ];
