465cd0982b9dd941b41fbf2b4b8f4261a68d875a
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-07-01 21:24:18 +0200

    Update script.js
    Removes training functions

--- script.js
+++ script.js
@@ -78,18 +78,4 @@ let userData = {
 	songCurrentTime: 0,
 };
 
-const printGreeting = () => {
-	console.log('Hello there!');
-};
 
-printGreeting();
-
-const printMessage = (org) => {
-	console.log(`${org} is awesome!`);
-};
-
-printMessage('freeCodeCamp');
-
-const addTwoNumbers = (num1, num2) => num1 + num2;
-
-console.log(addTwoNumbers(3, 4));
