feb97757263f896377287b224a79eb0e7e72fe08
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2024-07-01 21:21:59 +0200

    Update script.js
    Transforms function to implicit returns

--- script.js
+++ script.js
@@ -90,8 +90,6 @@ const printMessage = (org) => {
 
 printMessage('freeCodeCamp');
 
-const addTwoNumbers = (num1, num2) => {
-	return num1 + num2;
-};
+const addTwoNumbers = (num1, num2) => num1 + num2;
 
 console.log(addTwoNumbers(3, 4));
