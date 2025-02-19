f87d076b42d75db76f152655fbe10ccb8e74c371
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-02-19 10:13:59 +0100

    Update script.js
    Added if statement to make sure that before playing the song it starts from the beginning.

--- script.js
+++ script.js
@@ -78,11 +78,15 @@ let userData = {
 	songCurrentTime: 0,
 };
 
-const playSong = (id) =>{
-	const song = userData?.songs.find((song) => song.id === id)
+const playSong = (id) => {
+	const song = userData?.songs.find((song) => song.id === id);
 	audio.src = song.src;
 	audio.title = song.title;
-}
+
+	if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+		audio.currentTime = 0;
+	}
+};
 
 const renderSongs = (array) => {
 	const songsHTML = array
