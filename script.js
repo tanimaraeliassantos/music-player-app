5631fc5eff988fad9b4dce690d8e17bf50bb8599
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-03-24 14:24:10 +0100

    Update script.js
    Added pause method to pauseSong function

--- script.js
+++ script.js
@@ -96,6 +96,7 @@ const playSong = (id) => {
 const pauseSong = () => {
 	userData.songCurrentTime = audio.currentTime;
 	playButton.classList.remove("playing");
+	audio.pause();
 }
 
 const renderSongs = (array) => {
