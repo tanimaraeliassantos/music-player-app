5e6cb5012bc876195d78368105b7a7d5a89df7a9
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-03-27 14:59:56 +0100

    Update script.js
    Created constant previous song definition substracting one from current song index
    Added playsong with id from previous song constant

--- script.js
+++ script.js
@@ -114,6 +114,8 @@ const playPreviousSong = () => {
 		return
 	} else {
 		const currentSongIndex = getCurrentSongIndex();
+		const previousSong = userData?.songs[currentSongIndex - 1];
+		playSong(previousSong.id);
 	}
 };
 
