70553d3122286f88b6492e105b39ff7302217456
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-04-01 15:11:38 +0200

    Update script.js
    Created if block to check if there is a highlighted song

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ const highlightCurrentSong = () => {
 	playlistSongElements.forEach((songEl) => {
 		songEl.removeAttribute("aria-current");
 	});
+	if (songToHighlight){songToHighlight.setAttribute("aria-current", "true")};
 };
 
 
