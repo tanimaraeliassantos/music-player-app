201d213c2ace24ca2c527143bce6e3bbebbc2263
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-04-04 12:57:15 +0200

    Update script.js
    Called renderSongs to re-render songs

--- script.js
+++ script.js
@@ -126,6 +126,7 @@ const shuffle = () => {
 	userData?.songs.sort(() => Math.random() - 0.5);
 	userData.currentSong = null;
 	userData.songCurrentTime = 0;
+	renderSongs(userData?.songs);
 };
 
 const setPlayerDisplay = () => {
