0c230d0e0f07c276dc9430141a677127a65077e2
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-04-04 12:55:10 +0200

    Update script.js
    Set currentSong to nothing when shuffling

--- script.js
+++ script.js
@@ -123,28 +123,29 @@ const playPreviousSong = () => {
 };
 
 const shuffle = () => {
-	userData?.songs.sort(()=> Math.random() - 0.5);
-}
+	userData?.songs.sort(() => Math.random() - 0.5);
+	userData.currentSong = null;
+};
 
 const setPlayerDisplay = () => {
-	const playingSong = document.getElementById("player-song-title");
-	const songArtist = document.getElementById("player-song-artist");
+	const playingSong = document.getElementById('player-song-title');
+	const songArtist = document.getElementById('player-song-artist');
 	const currentTitle = userData?.currentSong?.title;
 	const currentArtist = userData?.currentSong?.artist;
-	playingSong.textContent = currentTitle ? currentTitle: "";
-	songArtist.textContent = currentArtist ? currentArtist: "";
+	playingSong.textContent = currentTitle ? currentTitle : '';
+	songArtist.textContent = currentArtist ? currentArtist : '';
 };
 
 const highlightCurrentSong = () => {
 	const playlistSongElements = document.querySelectorAll('.playlist-song');
 	const songToHighlight = getElementById(`song-${userData?.currentSong?.id}`);
 	playlistSongElements.forEach((songEl) => {
-		songEl.removeAttribute("aria-current");
+		songEl.removeAttribute('aria-current');
 	});
-	if (songToHighlight){songToHighlight.setAttribute("aria-current", "true")};
+	if (songToHighlight) {
+		songToHighlight.setAttribute('aria-current', 'true');
+	}
 };
-
-
 
 const renderSongs = (array) => {
 	const songsHTML = array
@@ -167,7 +168,10 @@ const renderSongs = (array) => {
 
 const setPlayButtonAccessibleText = () => {
 	const song = userData?.currentSong || userData?.songs[0];
-	playButton.setAttribute("aria-label", song?.title ? `Play ${song.title}` : "Play");
+	playButton.setAttribute(
+		'aria-label',
+		song?.title ? `Play ${song.title}` : 'Play'
+	);
 };
 
 const getCurrentSongIndex = () => {
