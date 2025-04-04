0d2a002c0c6b837734e8860e6ee534c21ee35797
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-04-04 12:51:36 +0200

    Update script.js
    Created function shuffle

--- script.js
+++ script.js
@@ -122,6 +122,10 @@ const playPreviousSong = () => {
 	}
 };
 
+const shuffle = () => {
+	
+}
+
 const setPlayerDisplay = () => {
 	const playingSong = document.getElementById("player-song-title");
 	const songArtist = document.getElementById("player-song-artist");
