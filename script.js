a888a65f8e5c585a20f2cb31ef929413baebf700
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-04-10 13:42:11 +0200

    Update script.js
    Set highlighting of current song and accessible text to occur

--- script.js
+++ script.js
@@ -135,6 +135,8 @@ const shuffle = () => {
 const deleteSong = (id) => {
 	userData.songs = userData?.songs.filter((song) => song.id!== id);
 	renderSongs(userData?.songs);
+	highlightCurrentSong();
+	setPlayButtonAccessibleText();
 }
 
 const setPlayerDisplay = () => {
